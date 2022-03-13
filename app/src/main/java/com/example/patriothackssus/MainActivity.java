d841669253825eaac6db622225eb27dd6c9d7286
Zxadeel <73723316+Zxadeel@users.noreply.github.com>
2022-03-13 09:31:40 -0400

    changed the fail visual stuff

--- app/src/main/java/com/example/patriothackssus/MainActivity.java
+++ app/src/main/java/com/example/patriothackssus/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.patriothackssus;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
+import android.os.Bundle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
+    PieChart pieChart;
+    PieData pieData;
+    PieDataSet pieDataSet;
+    ArrayList pieEntries;
+    ArrayList PieEntryLabels;
     int bottlecount = 0;
     int cancount = 0;
     int papercount = 0;
@@ -19,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pieChart = findViewById(R.id.pieChart);
+        getEntries();
+        pieDataSet = new PieDataSet(pieEntries, "");
+        pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        pieDataSet.setSliceSpace(2f);
+        pieDataSet.setValueTextColor(Color.WHITE);
+        pieDataSet.setValueTextSize(10f);
+        pieDataSet.setSliceSpace(5f);
         button = (Button) findViewById(R.id.button5);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,18 +50,26 @@ public class MainActivity extends AppCompatActivity {
         });
         helloTextView = (TextView) findViewById(R.id.textView10);
     }
-
-    protected  void onClick(View V){
+    private void getEntries() {
+        pieEntries = new ArrayList<>();
+        pieEntries.add(new PieEntry(bottlecount, 0));
+        pieEntries.add(new PieEntry(cancount, 1));
+        pieEntries.add(new PieEntry(papercount, 2));
 
     }
 
     public void increasebottlecount(View view) {
         bottlecount++;
+        pieEntries.set(0,bottlecount);
+        pieChart.invalidate();
         helloTextView.setText(Integer.toString(bottlecount));
     }
 
     public void increaespapercount(View view) {
         papercount++;
+        pieEntries.set(2,papercount);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
         helloTextView.setText("" + papercount );
     }
 
@@ -50,6 +80,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void increasecancount(View view) {
         cancount++;
+        pieEntries.set(1,cancount);
+        pieChart.setData(pieData);
         helloTextView.setText("" + cancount);
     }
 }
