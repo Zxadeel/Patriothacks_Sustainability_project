0fef0cc4975765a898bc65e3ea195ccbaf79d266
Bryan Ortiz <Bryanzarti@gmail.com>
2022-03-12 23:59:26 -0500

    what i have done so far,activity/put the succ into constraint mode

--- app/src/main/java/com/example/patriothackssus/MainActivity.java
+++ app/src/main/java/com/example/patriothackssus/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +13,18 @@ public class MainActivity extends AppCompatActivity {
     int cancount = 0;
     int papercount =0;
     TextView helloTextView;
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewActivity();
+            }
+        });
          helloTextView = (TextView) findViewById(R.id.textView10);
     }
 
--- app/src/main/java/com/example/patriothackssus/SUCC.java
+++ app/src/main/java/com/example/patriothackssus/SUCC.java
@@ -23,7 +23,7 @@ public class SUCC extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_succ);
 //        binding = ActivitySuccBinding.inflate(getLayoutInflater());
 //        setContentView(binding.getRoot());
 
